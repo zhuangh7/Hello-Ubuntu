e2920811e8407e3f0908d119d5580d480b685e65
Haoqi Zhuang <admin@zhuangh7.cn>
2020-11-12 15:31:33 +0800

    rename temp file

--- boom.sh
+++ boom.sh
@@ -3,8 +3,8 @@ set -e
 python3 Hello.py
 cp /etc/update-motd.d/00-header 00-header.bak
 #echo 'head -n 30 /etc/update-motd.d/00-header > /etc/update-motd.d/00-header' | sudo su
-echo 'head -n 30 /etc/update-motd.d/00-header > test' | sudo su
-echo 'cat out_ >> test' | sudo su
-sudo mv test /etc/update-motd.d/00-header
+echo 'head -n 30 /etc/update-motd.d/00-header > temp' | sudo su
+echo 'cat out_ >> temp' | sudo su
+sudo mv temp /etc/update-motd.d/00-header
 sudo chmod 755 /etc/update-motd.d/00-header
 
